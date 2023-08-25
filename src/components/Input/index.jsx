956f3acb6f071d1002c9316c722fdb4a27173e02
wasselucas <wasselucas.dev@gmail.com>
2023-08-24 22:27:40 -0300

    create input

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,19 @@
+import { styled } from "styled-components";
+
+export const Input = styled.input`
+
+	order: 1px solid #fff;
+	background:transparent;
+	border:1px solid #fff;
+	padding:1.2em 140px;
+	border-radius:50px;
+	width:200px;
+	color: #fff;
+	font-size:16px;
+	margin-bottom:1em;
+
+	& ::placeholder{
+		color:#fff;
+		font-size:16px;
+	}
+`
